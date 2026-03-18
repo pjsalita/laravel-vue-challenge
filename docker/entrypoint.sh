9d25dbf40beacde2947546967c8b9054d0d8e46f
Pj Salita <pj.salita@gmail.com>
2026-03-18 15:27:54 +0800

    chore: update entrypoint for deploying to a remote server

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,4 +12,15 @@ fi
 
 php artisan migrate:fresh --seed
 
+if [ -f public/hot ]; then
+    rm public/hot
+fi
+
+APP_ENV_VALUE="${APP_ENV:-}"
+if [ "$APP_ENV_VALUE" = "local" ] || [ "$APP_ENV_VALUE" = "development" ]; then
+    npm run dev &
+else
+    npm run build
+fi
+
 exec "$@"
